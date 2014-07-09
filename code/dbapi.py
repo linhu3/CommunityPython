395cafa309e22ce4066c145fb07fa0b160968e8f
AncientAnton <zx.anton95@gmail.com>
2014-07-09 10:55:08 +0800

    Update dbapi.py

--- code/dbapi.py
+++ code/dbapi.py
@@ -1,48 +1,60 @@
-# -*- coding:utf-8 -*-
-import MySQLdb
-import json
+import MySQLdb,json
+
+#in init function please change the config to fit your own requirement
+#fetchone(): return type: None/dict
+#fetchall(): return type: tuple(may be empty tuple)
+#all function below return a value or a list
 
 class dbapi:
 	def __init__(self):
-		#Change the database config to fit you own pc
 		self.host="localhost"
 		self.user="root"
 		self.passwd="root"
 		self.dbname="community"
 		self.charset="utf8"
 		self.db=MySQLdb.connect(host=self.host,user=self.user,passwd=self.passwd,db=self.dbname,charset=self.charset)
-    
-	#Get event By event's id
-	def getEventById(self,eventid):
-		cursor=self.db.cursor()
-		sql="""select * from event where id=%s"""
-		#Param must to tuple!1
+
+	def getUserByUserId(self,userid):
+		cursor=self.db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
+		sql="select * from user where id=%s"
+		param=(userid,)
+		cursor.execute(sql,param)
+		result=cursor.fetchone()
+		cursor.close()
+		return result
+
+	def getUserByUserName(self,username):
+		cursor=self.db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
+		sql="select * from user where name=%s"
+		param=(username,)
+		cursor.execute(sql,param)
+		result=cursor.fetchone()
+		cursor.close()
+		return result
+
+	def getEventByEventId(self,eventid):
+		cursor=self.db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
+		sql="select * from event where id=%s"
 		param=(eventid,)
 		cursor.execute(sql,param)
-		result=cursor.fetchall()
+		result=cursor.fetchone()
 		cursor.close()
 		return result
 
 	def getEventsByUserId(self,userid):
-		cursor=self.db.cursor()
+		cursor=self.db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
+		sql="select * from event where usrid=%s"
+		param=(userid,)
+		cursor.execute(sql,param)
+		result=cursor.fetchall()
 		cursor.close()
-		return 0
+		return list(result)
 
-        #register insert data
-	def regist(self,content):
-                #content = '{"username": "haha","password": 111111,"kind": 1, "cardid":11301 ,"realname":"hiii","sex":1,"age":41, "address":"iii","illness":"hijiiii"}'
-                #j = json.loads(content)
-                cursor = self.db.cursor()
-                sql = "insert into user(name,kind,password) values(%s,%s,%s)"
-                param = (content["username"],content["kind"],content["password"])
-                cursor.execute(sql,param)
-                self.db.commit()
-                print param
-                return sql
+	def getEventsByUserName(self,username):
+		user=self.getUserByUserName(username)
+		if(not user):
+			return []
+		return self.getEventsByUserId(user["userid"])
 
-        def hasuserName(self,username):
-                cursor = self.db.cursor()
-                sql = "select name from user where name = "
-    
 	def __del__(self):
 		self.db.close()
