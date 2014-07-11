0c9bc3edabd6591e1094134f7baba00a64176ad9
dage <641846382@qq.com>
2014-07-11 08:49:17 +0800

    2014/7/11 by linhu

--- code/handler/CheckrelativesHandler.py
+++ code/handler/CheckrelativesHandler.py
@@ -2,6 +2,22 @@ import tornado.ioloop
 import tornado.web
 import tornado.httpserver
 import os,dbapi
+import sys,json
+sys.path.append("..")
+
 class CheckrelativesHandler(tornado.web.RequestHandler):
-		def post(self):
-				self.write("CheckrelativesHandler")
+        def post(self):
+		username=self.get_argument("content")
+		userid=self.application.dbapi.getUserByUserName(username)["id"]
+		re=self.application.dbapi.CheckRelationbyId(userid)
+		if re!=[]:
+			relatives=[]
+			for row in re:
+				name=self.application.dbapi.getUsermassegeByUserId(row["cid"])
+				relatives.append(name)
+			data1=[{'state':1},relatives]
+			data=json.dumps(data1)
+		else:
+			data1=[{'state':0}]
+			data=json.dumps(data1)
+		self.write(data)
