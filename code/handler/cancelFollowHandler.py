1c8afe3e9f0c16d0b2daf35c9c6dad06c5fcca58
dage <641846382@qq.com>
2014-07-17 15:17:03 +0800

    2014/7/17 bylinhu 2.0

--- code/handler/cancelFollowHandler.py
+++ code/handler/cancelFollowHandler.py
@@ -8,19 +8,19 @@ class cancelFollowHandler(tornado.web.RequestHandler):
 		self.write("<p>cancelFollowHandler</p><form action='/api/cancelfollow' method='post'><input type='submit' value='submit'></form>")
 
 	def post(self):
-		content='{"id":2,"name":"ooo"}'
+		content='{"eid":2,"name":"ooo"}'
 		j=json.loads(content)
 		user=self.application.dbapi.getUserByUserName(j['name'])["id"]
 		if(user):
-			if(self.application.dbapi.getFollow(user["id"],j['id'])):
-				self.application.dbapi.delectFollow(user["id"],j['id'])
-				data=[{'state':1}]#delete follow success
+			if(self.application.dbapi.getFollow(user["id"],j['eid'])):
+				self.application.dbapi.delectFollow(user["id"],j['eid'])
+				data=[{'state':1},{'desc':"delete follow success"}]#delete follow success
 				result=json.dumps(data)
 			else:
-				data=[{'state':3}]#have no follow
+				data=[{'state':3},{'desc':"have no follow"}]#have no follow
 				result=json.dumps(data)
 		else:
-			data=[{'state':2}]#user no exist
+			data=[{'state':2},{'desc':"user no exist"}]#user no exist
 			result=json.dumps(data)
 		#return result
 
--- code/handler/startFollowHandler.py
+++ code/handler/startFollowHandler.py
@@ -8,18 +8,18 @@ class startFollowHandler(tornado.web.RequestHandler):
 		self.write("<p>startFollowHandler</p><form action='/api/startfollow' method='post'><input type='submit' value='submit'></form>")
 
 	def post(self):
-		content='{"id":2,"name":"ooo"}'
+		content='{"eid":2,"name":"ooo"}'
 		j=json.loads(content)
 		user=self.application.dbapi.getUserByUserName(j['name'])["id"]
 		if(user):
-			if(self.application.dbapi.getFollow(user["id"],j['id'])):
-				data=[{'state':3}]#have  been followed
+			if(self.application.dbapi.getFollow(user["id"],j['eid'])):
+				data=[{'state':3},{'desc':"have been followed"}]#have  been followed
 				result=json.dumps(data)
 			else:
-				self.application.dbapi.insertFollow(user["id"],j['id'])
-				data=[{'state':1}]#start follow success
+				self.application.dbapi.insertFollow(user["id"],j['eid'])
+				data=[{'state':1},{'desc':"start follow success"}]#start follow success
 				result=json.dumps(data)
 		else:
-			data=[{'state':2}]#user no exist
+			data=[{'state':2},{'desc':"user no exist"}]#user no exist
 			result=json.dumps(data)
 		#return result
